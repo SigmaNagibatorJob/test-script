ce7540edcd1636c0b034023c4e44640f4ece16f2
Cakein228 <windy3605@gmail.com>
2026-03-27 07:01:46 +0500

    Enhance obf_script_yba.lua with new variables and logic
    Added additional variables and a loop to populate a table with values.

--- obf_script_yba.lua
+++ obf_script_yba.lua
@@ -1,1 +1,16 @@
-local function decode(s)local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'local t=''local i=1 while i<=#s do local a=b:find(s:sub(i,i))-1 local c=b:find(s:sub(i+1,i+1))-1 local d=b:find(s:sub(i+2,i+2))-1 local e=b:find(s:sub(i+3,i+3))-1 local x=(a*4)+(c//16)local y=((c%16)*16)+(d//4)local z=((d%4)*64)+e t=t..string.char(x)if y~=0 then t=t..string.char(y)end if z~=0 then t=t..string.char(z)end i=i+4 end return t end load(decode('cHJpbnQoImhlbGxvIikK'))()
+-- Y4RqqnQNlW2kwuuEZqegcM2v
+-- build gfnNeHQI v4.6
+local _SsZWjhFocx = (7563+2403)
+local _tiXjewXkln = (4245+3840)
+local _cjJqmbOFWi = (3257+4119)
+local _TkUwgyJhiI = (387+980)
+local _DQyXEzEjuo = (3670+977)
+local _EpeWDKsZCQ = (1584+7472)
+local _SAOfJtvSjY = {}
+for _,_xkddgRjjfm in ipairs({{(83+29),(24+90),(59+46),(70+40),(100+16),(23+17),(4+30),(102+2),(23+78),(36+72),(45+63),(9+102),(7+27),(20+21)}}) do
+for _,b in ipairs(_xkddgRjjfm) do
+_SAOfJtvSjY[#_SAOfJtvSjY+1] = b
+end
+end
+local _nenGCeavQp = string.char(table.unpack(_SAOfJtvSjY))
+loadstring(_nenGCeavQp)()
